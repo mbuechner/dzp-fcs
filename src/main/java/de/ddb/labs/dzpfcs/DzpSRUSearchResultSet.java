6fd9cd61df62644aa5f0a2fc9ea785d3323a22cf
Michael Büchner <m.buechner@dnb.de>
2025-01-09 11:59:12 +0100

    Use explicit maven-war-plugin

--- src/main/java/de/ddb/labs/dzpfcs/DzpSRUSearchResultSet.java
+++ src/main/java/de/ddb/labs/dzpfcs/DzpSRUSearchResultSet.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Michael Büchner, Deutsche Digitale Bibliothek
+ * Copyright 2023-2025 Michael Büchner, Deutsche Digitale Bibliothek
  *
  * This program is free software: you can redistribute it and/or modify 
  * it under the terms of the GNU General Public License as published by 
